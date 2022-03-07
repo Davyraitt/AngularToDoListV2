056c7e6a93d7425428a74b000c79dd6a53bb2364
Davy <dj.raitt@student.avans.nl>
2022-03-07 12:41:45 +0100

    [PUSH] Added login authentication with AWS cognito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AmplifyAuthenticatorModule,
     BrowserModule
   ],
   providers: [],
